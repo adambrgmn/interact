d99db8b9993feece55da175da8d7b885c2d529d1
Adam Bergman <adam@fransvilhelm.com>
2018-11-14 22:39:50 +0000

    fix: Clear boilerplate

--- site/src/App.js
+++ site/src/App.js
@@ -1,28 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const handleSubmit = e => {
+  e.preventDefault();
+};
+
+function App() {
+  const [sessionId, setSessionId] = useState('');
+  const handleChange = e => setSessionId(Number.parseInt(e.target.value, 10));
+
+  return (
+    <div>
+      <header>
+        <div>
+          <h1>Interact</h1>
+          <h2>Let the audience ask the questions</h2>
+        </div>
+
+        <div>
+          <a href="/sign-in">Sign in</a>
+          {' | '}
+          <a href="/sign-up">Sign up</a>
+        </div>
+      </header>
+
+      <main>
+        <div>
+          <p>Enter an active session by providing the session number</p>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="sessionId">
+              <span>Enter Session ID:</span>
+              <span>#</span>
+              <input
+                type="number"
+                id="sessionId"
+                name="sessionId"
+                value={sessionId}
+                onChange={handleChange}
+              />
+            </label>
+
+            <button type="submit">Join session</button>
+          </form>
+        </div>
+
+        <div>
+          <p>Or create your own session</p>
+          <div>
+            <a href="/create">Create session</a>
+          </div>
+          <div>
+            <a href="/manage">Manage your sessions</a>
+          </div>
+        </div>
+      </main>
+
+      <footer>
+        <p>
+          Interact lets you engage your audience by opening up to questions but
+          with the benefit of knowing the questions on beforehand.
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
