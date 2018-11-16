92f93cba9aa3717c01a25a7a56ea0d5fca0c2634
Adam Bergman <adam@fransvilhelm.com>
2018-11-16 11:53:45 +0100

    fix: Styling Input

--- web/src/components/Input.js
+++ web/src/components/Input.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import styled from 'styled-components';
-import { borderRadius } from 'polished';
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import { lighten } from 'polished';
 
 const Label = styled.label`
-  display: flex;
   width: 100%;
-  flex-flow: row wrap;
 `;
 
 const LabelText = styled.span``;
@@ -13,38 +11,66 @@ const LabelText = styled.span``;
 const InputContainer = styled.div`
   display: flex;
   flex-flow: row nowrap;
-  width: 100%;
-  max-width: 22rem;
+  justify-content: flex-start;
+  align-items: center;
+  border: 1px solid ${p => p.theme.color.black};
+  border-radius: 4px;
+
+  ${p =>
+    p.inFocus &&
+    css`
+      border-color: ${p.theme.color.brand};
+    `}
 `;
 
 const InputPrefix = styled.span`
-  display: block;
-  border-right: none;
-  border: 1px solid ${p => p.theme.color.black};
-  ${borderRadius('left', '4px')};
-  padding: 0 0.5rem;
+  padding: 0 0.25rem 0 0.75rem;
   font-size: 1.5rem;
   line-height: 1.5;
   text-align: center;
+  vertical-align: middle;
+  color: ${p => lighten(0.3, p.theme.color.black)};
 `;
 
 const InputComp = styled.input`
   flex: 1;
-  ${borderRadius('right', '4px')};
-  border: 1px solid ${p => p.theme.color.black};
-  padding: 0;
+  width: 1px;
+  border: none;
+  padding: 0 0.75rem 0 0;
   font-size: 1.5rem;
   font-family: ${p => p.theme.font.family};
   line-height: 1.5;
+  vertical-align: middle;
+  color: ${p => p.theme.color.black};
+
+  &:focus {
+    outline: none;
+  }
 `;
 
-function Input({ id, label, prefix, className, ...props }) {
+function Input({ id, label, prefix, className, onFocus, onBlur, ...props }) {
+  const [inFocus, setFocus] = useState(false);
+
+  const handleFocus = e => {
+    setFocus(true);
+    if (onFocus) onFocus(e);
+  };
+  const handleBlur = e => {
+    setFocus(false);
+    if (onBlur) onBlur(e);
+  };
+
   return (
     <Label htmlFor={id}>
       <LabelText>{label}</LabelText>
-      <InputContainer>
+      <InputContainer inFocus={inFocus}>
         {prefix && <InputPrefix>{prefix}</InputPrefix>}
-        <InputComp id={id} {...props} />
+        <InputComp
+          id={id}
+          {...props}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        />
       </InputContainer>
     </Label>
   );
--- web/src/pages/Landing.js
+++ web/src/pages/Landing.js
@@ -1,21 +1,26 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import Input from '../components/Input';
+
+const Main = styled.main`
+  width: 100%;
+  max-width: 25rem;
+  padding: 0.5rem;
+`;
 
 function Landing() {
   const [sessionId, setSessionId] = useState('');
-  const handleChange = e => {
-    const num = Number.parseInt(e.target.value, 10);
-    if (!Number.isNaN(num)) setSessionId(num);
-  };
+  const handleChange = e => setSessionId(e.target.value);
   const handleSubmit = e => e.preventDefault();
 
   return (
-    <main>
+    <Main>
       <div>
         <p>Join an active session by providing the session number</p>
         <form onSubmit={handleSubmit}>
           <div>
             <Input
+              type="tel"
               id="sessionId"
               name="sessionId"
               label="Enter session id"
@@ -27,18 +32,18 @@ function Landing() {
 
           <button type="submit">Join session</button>
         </form>
+      </div>
 
+      <div>
+        <p>Or create your own session</p>
         <div>
-          <p>Or create your own session</p>
-          <div>
-            <a href="/create">Create session</a>
-          </div>
-          <div>
-            <a href="/manage">Manage your sessions</a>
-          </div>
+          <a href="/create">Create session</a>
+        </div>
+        <div>
+          <a href="/manage">Manage your sessions</a>
         </div>
       </div>
-    </main>
+    </Main>
   );
 }
 
